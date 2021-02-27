032984b770dd3f21a77d05f394b7cbfce400ffac
BaherW <73704436+BaherW@users.noreply.github.com>
2021-02-28 00:10:12 +0100

    Timing moves and call for audio implemented

--- background.js
+++ background.js
@@ -1,17 +1,77 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  });
+var username
+var waitTime
+var gameData
 
-chrome.storage.sync.get(["username"], function(items){
-    var username = items;
-    console.log(items)
-});
-chrome.storage.sync.get(["waitTime"], function(items){
-    var waitTime = items;
-    console.log(items)
-});
+function syncVars() {
+  chrome.storage.sync.get(["username", "waitTime"], function (results) {
+    username = results.username
+    waitTime = results.waitTime
+    console.log(waitTime)
+  })
+}
 
+function ourMain() {
+  let userColor;
+  let currentMoveColor;
+  let timer = 0;
+  setInterval(() => {
+    syncVars();
+    fetchData();
+    console.log("Fetching")
+    if (gameExists()) {
+      console.log("game exists")
+      if (gameData.players.white == username) {
+        userColor = "white"
+      }
+      else {
+        userColor = "black"
+      }
 
+      let moves = (gameData.moves).split(" ")
+      let movesLength = moves.length;
+      console.log(movesLength)
+      if (movesLength % 2 == 0) {
+        currentMoveColor = "white"
+      }
+      else {
+        currentMoveColor = "black";
+      }
 
+      if (currentMoveColor === userColor) {
+        console.log(timer)
+        timer++;
+      }
+      else {
+        timer = 0;
+      }
+      if (timer == waitTime) {
+        console.log("MOVE IDIOT")
+        var audio = new Audio('moveAudios/bahr1.mp4');
+        audio.play();
+      }
+    }
+  }, 1000)
+}
+
+function gameExists() {
+  if (gameData.status == "started") {
+    return true;
+  }
+  return false;
+}
+
+function fetchData() {
+  fetch("https://lichess.org/api/user/BahrW/current-game", {
+    method: 'GET',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.json())
+    .then(response => {
+      gameData = response
+    })
+}
+
+setTimeout(syncVars(), 5000)
+ourMain();
